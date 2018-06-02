d6a86ea41f9309fbe6e20383519b1ca1dd31ae7c
agustinmontero <agustinmontero87@gmail.com>
2018-06-02 15:21:58 -0300

    Eliminados archivos innecesarios

--- app/src/main/java/inti/recon/backend/Billete.java
+++ app/src/main/java/inti/recon/backend/Billete.java
@@ -10,46 +10,52 @@ import android.content.Context;
 
 public class Billete {
 
-	Mat frente;
-	Mat dorso;
-	MatOfKeyPoint keypointsf=new MatOfKeyPoint();
-	MatOfKeyPoint keypointsd = new MatOfKeyPoint();
-	Mat descriptorsf = new Mat();
-	Mat descriptorsd = new Mat();
-	
-	
-	public Billete(Context context,Mat f,Mat d){
-		DescriptorExtractor descriptor = DescriptorExtractor.create(DescriptorExtractor.ORB);
-		FeatureDetector detector = FeatureDetector.create(FeatureDetector.ORB);
-		
-		frente=f.clone();
-		dorso=d.clone();
-			
-		detector.detect(frente, keypointsf);
-		descriptor.compute(frente, keypointsf, descriptorsf);
-		
-		detector.detect(dorso, keypointsd);
-		descriptor.compute(dorso, keypointsd, descriptorsd);
-			
-	}
-	public Mat getFrente(){
-		return frente;
-	}
-	public Mat getDorso(){
-		return dorso;
-	}
-	public MatOfKeyPoint getKFrente(){
-		return keypointsf;
-	}
-	public MatOfKeyPoint getKDorso(){
-		return keypointsd;
-	}
-	public Mat getDFrente(){
-		return descriptorsf;
-	}
-	public Mat getDDorso(){
-		return descriptorsd;
-	}
-	
-	
+    private Mat frente;
+    private Mat dorso;
+    private MatOfKeyPoint keypointsf = new MatOfKeyPoint();
+    private MatOfKeyPoint keypointsd = new MatOfKeyPoint();
+    private Mat descriptorsf = new Mat();
+    private Mat descriptorsd = new Mat();
+
+
+    public Billete(Context context, Mat f, Mat d) {
+        DescriptorExtractor descriptor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+        FeatureDetector detector = FeatureDetector.create(FeatureDetector.ORB);
+
+        frente = f.clone();
+        dorso = d.clone();
+
+        detector.detect(frente, keypointsf);
+        descriptor.compute(frente, keypointsf, descriptorsf);
+
+        detector.detect(dorso, keypointsd);
+        descriptor.compute(dorso, keypointsd, descriptorsd);
+
+    }
+
+    public Mat getFrente() {
+        return frente;
+    }
+
+    public Mat getDorso() {
+        return dorso;
+    }
+
+    public MatOfKeyPoint getKFrente() {
+        return keypointsf;
+    }
+
+    public MatOfKeyPoint getKDorso() {
+        return keypointsd;
+    }
+
+    public Mat getDFrente() {
+        return descriptorsf;
+    }
+
+    public Mat getDDorso() {
+        return descriptorsd;
+    }
+
+
 }
